7e5a3177d9a7d9f45c228387e6d800c00012d96a
Burhanuddin Rashid <burhanuddin@shiny.team>
2024-01-26 18:59:02 +0530

    Records

---
+++ source-code/04_pattern_matching_records/01_records.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  final name = (firstName: "John", lastName: "Smith");
+  switch (name) {
+    case (firstName: "John", lastName: "Smith"):
+      print("Found name");
+  }
+}
